d40451052c1c9ca7b6cc94c15a072118f67e8365
hbuzzaman <hbuzzaman@gmail.com>
2022-08-12 16:19:38 +0700

    quis

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// makeCoffee("robusta", 80);
+
+import {coffeeStock as sStock, isCoffeeMachineReady} from './state.js';
+ 
+// const displayStock = stock => {
+//     for (const type in stock) {
+//         console.log(type);
+//     }
+// }
+ 
+// displayStock(coffeeStock);
+
+// const coffeeStock = require('./state');
+console.log(sStock);
+console.log(isCoffeeMachineReady);
